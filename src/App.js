668ef7be426bac864f593750bdbb6098fe469821
Bo-vcu <99736481+Bo-vcu@users.noreply.github.com>
2022-12-08 16:15:12 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import CollectionForm from './Form'
 
 class App extends Component{
 
-    API_URL="http://193.191.176.86:8083/"
+    API_URL="http://193.191.176.86:8080/"
 
     state = {
       products: [],
